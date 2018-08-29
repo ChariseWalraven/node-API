bff554e0eeff1403d0df7761f2003a4dcc6177a2
Charise Walraven <cherri.wlraven@gmail.com, walraven@charise.info>
2018-08-29 17:58:51 +0200

    too long ago to remember what exactly i did

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-var app = require('express')(),
-  port = process.env.PORT || 3000;
+const app = require("express")(),
+     port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-  res
-  	.json({ 
-  		data: 'Working condition' 
-  	});
+app.get("/", (req, res) => {
+  res.json({
+    data: "Working condition"
+  });
 });
 
-app.get("/happy", function(req, res) {
-  res
-  	.send(":)");
-})
+app.get("/happy", (req, res) => {
+  res.send(":)");
+});
 
-app.listen(port, console.log('App is running on port ' + port));
+app.listen(port, console.log("App is running on port " + port));
