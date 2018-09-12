531a11c7fc077c64bce90f8676065287db9e9281
Charise Walraven <cherri.wlraven@gmail.com, walraven@charise.info>
2018-09-12 17:14:22 +0200

    add the routes folder and add a basic route

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const app = require("express")();
 const port = process.env.PORT || 3000;
 
+const todoRoutes = require("./routes/todos");
+
+// todos routes
 app.get("/", (req, res) => {
-  res.json({
-    data: "Working condition"
-  });
+  res.send("Hello from the root route!");
 });
 
-app.get("/happy", (req, res) => {
-  res.send(":)");
-});
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, console.log("App is running on port " + port));
